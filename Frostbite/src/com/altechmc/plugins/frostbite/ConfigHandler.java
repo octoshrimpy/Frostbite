4051304c76db52d02eba7da979e05603ee0bea11
robocoder99 <admin@altechmc.com>
2014-10-14 07:07:31 -0400

    Commit!

--- Frostbite/src/com/altechmc/plugins/frostbite/ConfigHandler.java
+++ Frostbite/src/com/altechmc/plugins/frostbite/ConfigHandler.java
@@ -15,6 +15,29 @@ public class ConfigHandler {
     
     public ConfigHandler(){
         FileConfiguration conf = Frostbite.getInstance().getConfig();
-        //TODO: Get Configs
+
+        List<String> hlist = conf.getStringList("HeatBlocks");
+        for(String s: hlist){
+            String smat = conf.getString("HeatBlocks." + s + ".Type");
+            int samount = conf.getInt("HeatBlocks." + s + ".Rate");
+            int srange = conf.getInt("HeatBlocks." + s + ".Range");
+            Material mat = Material.getMaterial(smat);
+            int amount = samount;
+            int range = samount;
+            Integer[] dat = {amount, range};
+            heatblocks.put(mat, dat);
+        }
+        
+        List<String> clist = conf.getStringList("CoolBlocks");
+        for(String s: clist){
+            String smat = conf.getString("CoolBlocks." + s + ".Type");
+            int samount = conf.getInt("CoolBlocks." + s + ".Rate");
+            int srange = conf.getInt("CoolBlocks." + s + ".Range");
+            Material mat = Material.getMaterial(smat);
+            int amount = samount;
+            int range = samount;
+            Integer[] dat = {amount, range};
+            coolblocks.put(mat, dat);
+        }
     }
 }
