3fa712638a5cd31ae820b1c8df152f9a965c9f92
robocoder99 <admin@altechmc.com>
2014-10-14 07:54:19 -0400

    Commits...

---
+++ Frostbite/src/com/altechmc/plugins/frostbite/Util.java
@@ -0,0 +1,13 @@
+package com.altechmc.plugins.frostbite;
+
+public class Util {
+	
+	public static void updateXPBar(int stat){
+		//TODO: Set XP Bar
+	}
+	
+	public static void updatePlayerEffects(int stat){
+		
+	}
+
+}
